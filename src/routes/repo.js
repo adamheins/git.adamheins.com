4c6f8f7a1c19a946cde60b4e09705452f4f7d344
Adam Heins <mail@adamheins.com>
2016-11-15 17:20:25 -0500

    Fix crash with no README.
    Rewrote repo promise code so that the README is found at the end of the
    process. Repos no longer require a README to behave properly. Code was
    also considerably cleaned up.

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -15,7 +15,43 @@ let util = require('../lib/util');
 let Sha = require('../lib/sha');
 
 
+function readmeFromBlob(blob, renderer) {
+    let readme = blob.toString();
+
+    // Parse the README from markdown into html.
+    try {
+        return marked(readme, {
+            renderer: renderer
+        });
+    } catch(err) {
+        log.info('README parsing error.');
+        log.error(err);
+        return readme;
+    }
+    return '';
+}
+
+
+function renderRepo(req, res, tree, readme, lastCommit) {
+    // Generate links to all contents of the repository at the head of
+    // the master branch.
+    let repoContentBaseUrl = [req.originalUrl, 'files', 'master'].join('/');
+
+    let repo = req.repo;
+    repo.readme = readme;
+    repo.entries = util.mapTreeEntries(tree.entries(), repoContentBaseUrl);
+
+    lastCommit.then(lastCommit => {
+        res.render('repo', {
+            repo: repo,
+            lastCommit: lastCommit
+        });
+    }).done();
+}
+
+
 router.get('/', (req, res, next) => {
+    // Get the head commit of the repository.
     let commit = git.Repository.open(req.repo.path).then(repo => {
         return repo.getHeadCommit();
     }, err => {
@@ -25,68 +61,42 @@ router.get('/', (req, res, next) => {
         throw error;
     });
 
+    // Gather information about the last commit.
+    let lastCommit = commit.then(commit => {
+        let author = commit.author();
+        let date = moment(commit.date());
+        let sha = new Sha(commit.sha());
+        return {
+            author: author.name(),
+            date: date.format('MMM D, YYYY'),
+            sha: sha
+        };
+    });
+
     commit.then(commit => {
-        return commit.getEntry('README.md');
-    }).then(entry => {
-        return entry.getBlob();
-    }).then(blob => {
-
-        let author = '';
-        let date = '';
-        let sha = '';
-
+        return commit.getTree();
+    }).then(tree => {
         commit.then(commit => {
-            author = commit.author();
-            date = moment(commit.date());
-            sha = new Sha(commit.sha());
-
-            return commit.getTree();
-        }).then(tree => {
-            let readme = blob.toString();
-
-            // Parse the README from markdown into html.
-            try {
-                readme = marked(readme, {
-                    renderer: renderer.getRenderer(req.repo.name)
-                });
-            } catch(err) {
-                log.info('README parsing error.');
+            return commit.getEntry('README.md');
+        }).then(entry => {
+            return entry.getBlob();
+        }).then(blob => {
+            // Have README.
+            let readme = readmeFromBlob(blob, renderer.getRenderer(req.repo.name));
+            renderRepo(req, res, tree, readme, lastCommit);
+        }, err => {
+            // No README.
+            // Assume that errors that aren't 404's indicate that the README is not
+            // present, but we can otherwise go ahead.
+            if (err.status !== 404) {
+                log.info('No README.');
                 log.error(err);
+
+                renderRepo(req, res, tree, '', lastCommit);
+            } else {
+                next(err);
             }
-
-            // Generate links to all contents of the repository at the head of
-            // the master branch.
-            let repoContentBaseUrl = [req.originalUrl, 'files', 'master'].join('/');
-
-            let repo = req.repo;
-            repo.readme = readme;
-            repo.entries = util.mapTreeEntries(tree.entries(), repoContentBaseUrl);
-
-            res.render('repo', {
-                repo: repo,
-                lastCommit: {
-                    author: author.name(),
-                    date: date.format('MMM D, YYYY'),
-                    sha: sha
-                }
-            });
         }).done();
-    }, err => {
-        // Assume that errors that aren't 404's indicate that the README is not
-        // present, but we can otherwise go ahead.
-        if (err.status !== 404) {
-            log.info('No README.');
-            log.error(err);
-
-            let repo = req.repo;
-            repo.readme = 'No README.';
-
-            res.render('repo', {
-                repo: repo
-            });
-        } else {
-            next(err);
-        }
     }).done();
 });
 
