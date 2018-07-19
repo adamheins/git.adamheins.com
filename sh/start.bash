ae9aff7fb099c30fb4fbe44af0744aa7df1f98c6
Adam Heins <mail@adamheins.com>
2018-07-18 20:46:33 -0400

    Add more descriptive comments to control scripts.

--- sh/start.bash
+++ sh/start.bash
@@ -11,9 +11,12 @@ while read line; do export "$line";
 done < .env
 
 if [ -f "$PID_FILE" ]; then
+  # If a PID file exists, that means the process is potentially running, so we
+  # do a restart to stop it first.
   echo "PID file found, restarting..."
   npm restart
 else
+  # No PID file exists, start up the process and generate it.
   nohup node src/server.js >/dev/null 2>&1 &
   echo $! > "$PID_FILE"
 fi
--- sh/stop.bash
+++ sh/stop.bash
@@ -7,9 +7,12 @@ cd $GIT_SITE_ROOT
 PID_FILE=$GIT_SITE_ROOT/run/git.adamheins.com.pid
 
 if [ -f "$PID_FILE" ]; then
+  # Kill the process indicated by the PID file and remove it.
   kill -9 $(cat "$PID_FILE")
   rm -f "$PID_FILE"
   echo "Removed PID file."
 else
+  # If there isn't a PID file, either the process isn't running or something
+  # has gone wrong in generating it.
   echo "No PID file found."
 fi
